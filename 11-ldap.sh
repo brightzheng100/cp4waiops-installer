ffb4141b8ee41e40b623441f62fe4d1ea2337d9e
Bright Zheng <bright.zheng@outlook.com>
2021-03-18 12:28:04 +0800

    use configurable variable for the ldap ns

--- 11-ldap.sh
+++ 11-ldap.sh
@@ -8,15 +8,15 @@ source lib/status.sh
 #
 function install-ldap {
     # Create a dedicated namespace
-    execlog oc new-project ldap || true
+    execlog oc new-project $NAMESPACE_LDAP || true
 
     # Assign scc to ldap/default sa
-    execlog oc adm policy add-scc-to-user anyuid system:serviceaccount:ldap:default
+    execlog oc adm policy add-scc-to-user anyuid system:serviceaccount:$NAMESPACE_LDAP:default
 
     # Install OpenLDAP
-    execlog oc -n ldap create secret generic openldap --from-literal=adminpassword=$LDAP_ADMIN_PASSWORD
+    execlog oc -n $NAMESPACE_LDAP create secret generic openldap --from-literal=adminpassword=$LDAP_ADMIN_PASSWORD
     # deploy
-    execlog oc -n ldap apply -f integration/ldap/openldap.yaml
+    execlog oc -n $NAMESPACE_LDAP apply -f integration/ldap/openldap.yaml
 }
 
 #
@@ -24,15 +24,15 @@ function install-ldap {
 #
 function install-ldap-post {
     # Initialize the LDAP data
-    local POD=$(oc -n ldap get pod -l app.kubernetes.io/name=openldap -o jsonpath="{.items[0].metadata.name}")
+    local POD=$(oc -n $NAMESPACE_LDAP get pod -l app.kubernetes.io/name=openldap -o jsonpath="{.items[0].metadata.name}")
     # Copy over the ldif files
-    execlog oc cp integration/ldap/ldif ldap/$POD:/
-    execlog oc -n ldap exec $POD -- ls /ldif
+    execlog oc cp integration/ldap/ldif $NAMESPACE_LDAP/$POD:/
+    execlog oc -n $NAMESPACE_LDAP exec $POD -- ls /ldif
     # Run the ldif files
-    execlog oc -n ldap exec $POD -- ldapadd -x -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -H ldap:// -f /ldif/0-ous.ldif
-    execlog oc -n ldap exec $POD -- ldapadd -x -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -H ldap:// -f /ldif/1-users.ldif
-    execlog oc -n ldap exec $POD -- ldapadd -x -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -H ldap:// -f /ldif/2-groups.ldif
+    execlog oc -n $NAMESPACE_LDAP exec $POD -- ldapadd -x -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -H ldap:// -f /ldif/0-ous.ldif
+    execlog oc -n $NAMESPACE_LDAP exec $POD -- ldapadd -x -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -H ldap:// -f /ldif/1-users.ldif
+    execlog oc -n $NAMESPACE_LDAP exec $POD -- ldapadd -x -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -H ldap:// -f /ldif/2-groups.ldif
 
     # Verify the entities
-    execlog oc -n ldap exec $POD -- ldapsearch -LLL -x -H ldap:// -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -b "ou=people,dc=bright,dc=com" dn
+    execlog oc -n $NAMESPACE_LDAP exec $POD -- ldapsearch -LLL -x -H ldap:// -D "cn=admin,dc=bright,dc=com" -w $LDAP_ADMIN_PASSWORD -b "ou=people,dc=bright,dc=com" dn
 }
